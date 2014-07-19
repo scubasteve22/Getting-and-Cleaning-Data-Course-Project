21391a5cfc737a881bc0f8b057043bb2439c8c5c
scubasteve22 <sjwyer@googlemail.com>
2014-07-19 11:44:50 +1000

    Final version of run_analysis.R
    Still need to update the .md files

---
+++ README.Rmd
@@ -0,0 +1,18 @@
+# Getting and Cleaning Data - Course Project
+
+Please find below the code used borken into key segments with explanations of each section.  Within the current directory there are the files below:
+
+1. Dataset.zip - The original data downloaded for the exercise
+2. run_analysis.R - The R file to complete the exercise
+3. gettingandcleaningdata.html - html version of the knitr output
+4. gettingandcleaningdata.Rmd - The R Markdown file to explian the code used
+5. README.md - The file to explain the construction and use of the code
+6. tidy_data.txt - The file output to meet the first tidy data requirement
+7. tidy_data2.txt - The file output to meet the first tidy data requirement
+8. UCI HAR Dataset folder/ - Holds the unzipped data required
+
+Below explains how they connect:
+
+
+
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,6 @@
 ## Course Project - Scuba22
 
-# Ensure all necessary libraries are read in (some may not be needed in final cut)
-library(knitr)
-
-opts_chunk$set(warning = FALSE, message = FALSE, echo = TRUE, 
-               fig.width = 10)
-
-Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.  `warning = FALSE` and `message = FALSE` are added to remove the uncessary warnings and messages when producing the plots.  Finally added `fig.width = 10` to make the plots wider and easier to read.
-
 ### Read in raw data
-This stage reads in the .zip file and then unzips ready for use:
-  ```{r, read.data}
 # Set the location of the data to use
 fileurl<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
@@ -82,11 +72,47 @@ test_data<-cbind("Subject Type" = "Test",
 rm(test_subject, test_set, test_activity, test_labels)
 rm(features, activity_labels)
 
-# Merge the 2 datasets into one tidy table
-tidy_data <- rbind(test_data, training_data)
+# Merge the 2 datasets into one tidy table and sort the table to make it readable, for this table I have decided by Subject is most relevant to see what each subject undertook
+tidy_data_base <- rbind(test_data, training_data)
+tidy_data <- tidy_data_base[order(tidy_data_base$"Subject", tidy_data_base$"Activity"),]
+
+# Change the factors to chars
+i <- sapply(tidy_data, is.factor)
+tidy_data[i] <- lapply(tidy_data[i], as.character)
 
 # Remove all unecessary data
-rm(test_data, training_data)
+rm(test_data, training_data, i, tidy_data_base)
 
-# Output the .txt file (comma delimited)
-write.table(tidy_data, file = "tidy_data.txt", sep = ",")
+# Output the .txt file (comma delimited) - Set row.names to false to prevent exporting row numbers and confusing column names
+write.table(tidy_data, file = "tidy_data.txt", sep = ",", row.names = FALSE)
+
+
+
+
+
+### Create the mean data for the second independent table
+agg_base<-aggregate(tidy_data, by=list(tidy_data$Activity, tidy_data$Subject), FUN=mean)
+
+### Remove the unwanted columns and rename the groupings
+
+# Identify the columns to drop
+drops <- c("Subject Type", "Subject", "Activity")
+
+# Recreate the table without them (and remove the drops data)
+agg_base<-agg_base[,!(names(agg_base) %in% drops)]
+rm(drops)
+
+# Rename the group columns for clarity; both by adding _MEAN and changing the groupings
+colnames(agg_base) <- paste(colnames(agg_base), "MEAN", sep = "_")
+agg_base<-rename(agg_base, c("Group.1_MEAN"="Activity", "Group.2_MEAN"="Subject"))
+
+# Sort the order to make it readable (and remove the previous data) - Order this time by activity to understand impact better across subjects
+tidy_data_mean <- agg_base[order(agg_base$"Activity", agg_base$"Subject"),]
+rm(agg_base)
+
+# Output the .txt file (comma delimited) - Set row.names to false to prevent exporting row numbers and confusing column names
+write.table(tidy_data_mean, file = "tidy_data_mean.txt", sep = ",", row.names = FALSE)
+
+# Produces the knitr output required (not included in the R Markdown files)
+knit("README.Rmd")   
+knit("CookBook.Rmd")   
